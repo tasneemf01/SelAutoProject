8cae09ef28b742bc5c9454548a61d9d2fb10348a
Tasneem <Tasneem@LAPTOP-GAF35F4H>
2025-08-07 17:11:34 -0400

    if example practice

---
+++ javalearning/src/javaconcepts/IfExamample.java
@@ -0,0 +1,10 @@
+package javaconcepts;
+
+public class IfExamample {
+	public static void main (String[] args) {
+	int age = 12; 
+	if (age >= 16); {
+		System.out.println("You are eligible to drive");	
+	} 
+	}
+}
