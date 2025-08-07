1e406a395f4c7a307a842c7a613284803bcc03a9
Tasneem <Tasneem@LAPTOP-GAF35F4H>
2025-08-07 17:11:17 -0400

    if else example

---
+++ javalearning/src/javaconcepts/IfElseExample.java
@@ -0,0 +1,13 @@
+package javaconcepts;
+
+public class IfElseExample {
+	
+	public static void main(String[] args) {
+	int marks=70;
+	if (marks>=50) {
+		System.out.println("You have passed");
+	}   else {
+		System.out.println("You have failed");
+	}
+}
+}
