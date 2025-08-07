ba847e1740905603233b552088bad7e804f9cabb
Tasneem <Tasneem@LAPTOP-GAF35F4H>
2025-08-07 17:37:25 -0400

    changed marks

--- javalearning/src/javaconcepts/IfElseExample.java
+++ javalearning/src/javaconcepts/IfElseExample.java
@@ -3,7 +3,7 @@ package javaconcepts;
 public class IfElseExample {
 	
 	public static void main(String[] args) {
-	int marks=70;
+	int marks=80;
 	if (marks>=50) {
 		System.out.println("You have passed");
 	}   else {
