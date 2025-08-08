5af92f2962dadae77fbd86b27621f54b6df77f80
Tasneem <Tasneem@LAPTOP-GAF35F4H>
2025-08-07 23:35:54 -0400

    else if statement practice

---
+++ javalearning/src/javaconcepts/ElseIfStatementPractice.java
@@ -0,0 +1,22 @@
+package javaconcepts;
+
+public class ElseIfStatementPractice {
+	
+public static void main (String[]args) {
+
+	int score= 85;
+	
+	if(score>=90) {
+	System.out.println("Grade A");
+	} else if (score >= 75)    {
+	System.out.println("Grade B");
+	} else if (score>=60)    {
+	System.out.println("Grade C");
+	} else {
+	System.out.println("Grade F");
+}
+}
+}
+
+
+
