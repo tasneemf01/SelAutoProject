49f4efc5987d889876fba2a00868374e52e77a1e
Tasneem <Tasneem@LAPTOP-GAF35F4H>
2025-08-15 23:28:34 -0400

    For loop

---
+++ javalearning/src/javaconcepts/ForLoopPractice.java
@@ -0,0 +1,19 @@
+package javaconcepts;
+
+public class ForLoopPractice{
+	
+	public static void main(String [] args) {
+			// reverse count
+		for(int i=10; i>0; i--){
+			
+			System.out.println(i);
+	
+		}
+		// forward count
+		for(int i=1; i<=10; i++){
+			
+			System.out.println(i);
+	
+		}
+	}
+}
