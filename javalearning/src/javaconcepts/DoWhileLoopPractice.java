f164cd7953d5375fe81f213b6d4e7bcab3bbc9b8
Tasneem <Tasneem@LAPTOP-GAF35F4H>
2025-08-15 23:37:20 -0400

    Do While Loop

---
+++ javalearning/src/javaconcepts/DoWhileLoopPractice.java
@@ -0,0 +1,20 @@
+package javaconcepts;
+
+public class DoWhileLoopPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i = 10;
+		
+			do{
+				
+				System.out.println(i);
+				i--;
+			}while(i>0);
+			
+	}
+
+}
+
+
