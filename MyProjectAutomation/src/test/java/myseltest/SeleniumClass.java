5ea250cbf257449a94f6f8ded334280251fcfa8c
Tasneem <Tasneem@LAPTOP-GAF35F4H>
2025-08-19 20:13:49 -0400

    Rremove Comments

--- MyProjectAutomation/src/test/java/myseltest/SeleniumClass.java
+++ MyProjectAutomation/src/test/java/myseltest/SeleniumClass.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-//  Class definitions
+
 public class SeleniumClass {
 
 	public static void main(String[] args) {
